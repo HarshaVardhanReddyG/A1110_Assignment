fb2e3d3cd283927022485bb9e0bc57e5d21f6d6b
HarshaVardhanReddyG <102507828+HarshaVardhanReddyG@users.noreply.github.com>
2022-07-01 19:09:55 +0530

    Add files via upload

--- codes/cdf_other_plot.py
+++ codes/cdf_other_plot.py
@@ -1,22 +1,27 @@
+#Importing numpy, scipy, mpmath and pyplot
 import numpy as np
+import matplotlib
+matplotlib.rcParams['backend'] = 'GTK3Agg'
 import matplotlib.pyplot as plt
 
 
-x = np.linspace(-4,4,30)#points on the x axis
+x = np.linspace(-10,10,50)#points on the x axis
+x1 = np.linspace(-10,0,50)
+y1 = np.zeros(50)
+x2 = np.linspace(0,10,50)
+y2 = 1 - np.exp(-x2/2)
 simlen = int(1e6) #number of samples
 err = [] #declaring probability list
-#randvar = np.random.normal(0,1,simlen)
 randvar = np.loadtxt('other.dat',dtype='double')
-#randvar = np.loadtxt('gau.dat',dtype='double')
-for i in range(0,30):
+for i in range(0,50):
 	err_ind = np.nonzero(randvar < x[i]) #checking probability condition
 	err_n = np.size(err_ind) #computing the probability
 	err.append(err_n/simlen) #storing the probability values in a list
 
-	
-plt.plot(x.T,err)#plotting the CDF
+plt.plot(x,err,'bo')#plotting the CDF
+plt.plot(np.concatenate([x1,x2]), np.concatenate([y1,y2]))
 plt.grid() #creating the grid
-plt.xlabel('$x$')
-plt.ylabel('$F_X(x)$')
-plt.savefig('other_cdf.pdf')
-plt.savefig('other_cdf.eps')
+plt.xlabel('$v$')
+plt.ylabel('$F_V(v)$')
+plt.legend(["Numerical","Theory"])
+plt.savefig('../figs/other_cdf.pdf')
--- codes/mean_variance.c
+++ codes/mean_variance.c
@@ -1,35 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include "coeffs.h"
 
 int main(){
-    FILE *fp;
-    fp=fopen("uni.dat","r");
-    double sum=0;
-    double a=0;
-    int i=0;
-    double m=0;
-    while(fscanf(fp,"%lf",&a)!=-1){
-        sum+=a;i++;
-    }
-    fclose(fp);
-    //printf("%d\n",i);
-    if(i==1000000){
-        m=sum/1000000;
-        printf("mean = %lf\n",m);
-    }
-    fp=fopen("uni.dat","r");
-    double b=0;
-    double sq=0;
-    i=0;
-    //printf("%lf\n",m);
-    while(fscanf(fp,"%lf",&b)!=-1){
-        sq+=(b-m)*(b-m);i++;
-    }
-    fclose(fp);
-    
-    if(i==1000000){
-        double v=sq/1000000;
-        printf("variance = %lf\n",v);
-    }
+    double Mean = mean("uni.dat");
+    double Variance = variance("uni.dat");
+    printf("Mean = %lf\n Variance = %lf\n",Mean,Variance);
     return 0;
 }
